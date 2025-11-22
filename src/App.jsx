cc3f415c21960331909bf5727e91e5b3313f5df0
Daksh Dhok <dakshdhok18@gmail.com>
2025-11-22 22:24:47 +0530

    ADDED BUTTONS

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ function App() {
   const handleIncrement = () => setCount(count + 1);
   const handleDecrement = () => setCount(count - 1);
   const handleReset = () => setCount(0);
+  const handleIncrementby5 = () => setCount(count + 5);
+  const handleDecrementby5 = () => setCount(count - 5);
 
   return (
     <div className="app-container">
@@ -15,9 +17,11 @@ function App() {
       <div className="count-display">{count}</div>
 
       <div className="button-row">
-        <button onClick={handleDecrement}>-1</button>
-        <button onClick={handleReset}>Reset</button>
-        <button onClick={handleIncrement}>+1</button>
+        <button onClick = {handleDecrement}> -1 </button>
+        <button onClick = {handleReset}> Reset </button>
+        <button onClick = {handleIncrement}> +1 </button>
+        <button onClick = {handleIncrementby5}> +5 </button>
+        <button onClick = {handleDecrementby5}> -5 </button>
       </div>
     </div>
   );
